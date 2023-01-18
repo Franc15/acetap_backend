1fa833378a1940675247e022ea6e1b50bb67aa6c
Francis Kikulwe <francissteven1@gmail.com>
2023-01-18 18:36:01 +0400

    add user and link models

--- models.py
+++ models.py
@@ -21,7 +21,54 @@ setup_db(app)
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
     db.app = app
     db.init_app(app)
     db.create_all()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    fname = db.Column(db.String(120))
+    lname = db.Column(db.String(120))
+    email = db.Column(db.String(350), unique=True)
+    gender = db.Column(db.String(10))
+    phone = db.Column(db.String(20))
+    dob = db.Column(db.Date())
+    links = db.relationship('Link', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.id}, name: {self.fname, self.lname} email: {self.email} >'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.fname + ' ' + self.lname,
+            'email': self.email,
+            'gender': self.gender,
+            'phone': self.phone,
+            'dob': self.dob
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Link(db.Model):
+    __tablename__ = 'links'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(500))
+    url = db.Column(db.String(500))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'url': self.url
+        }
+
+    
+
 
