619ade40b4d0548fdd0b96be5cd37baae3038b93
Francis Kikulwe <francissteven1@gmail.com>
2023-01-18 20:17:13 +0400

    add sample db data

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 
-from models import setup_db
+from models import setup_db, fill_db, User, Link
 
 
 # create and configure the app
 app = Flask(__name__)
 app.app_context().push()
 setup_db(app)
+fill_db()
 
 CORS(app)
 
@@ -17,11 +18,27 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
     return response
 
+
+
 @app.route('/')
 def index():
     return jsonify({
         'success': True,
         'message': 'Hello World'
     })
+
+@app.route('/links/<username>')
+def get_links(username):
+    user = User.query.filter_by(username=username).first()
+    # user = User.query.filter_by(id=id).one_or_404()
+    if not user:
+        abort(404)
+    links = Link.query.filter_by(user_id=user.id).all()
+
+    return jsonify({
+        'success': True,
+        'links': [link.serialize() for link in links]
+    })
+
 
 
--- models.py
+++ models.py
@@ -24,13 +24,41 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_ECHO"] = True
     db.app = app
     db.init_app(app)
+    db.drop_all()
     db.create_all()
+
+def fill_db():
+    user1 = User(fname='John', lname='Doe', email='johndoe@gmail.com', gender='male',
+        username='john.doe', phone='1234567890', dob='1990-2-10')
+    user2 = User(fname='Jane', lname='Doe', email='janedoe@gmail.com', gender='female',
+        username='jane.doe', phone='1234567890', dob='1992-1-1')
+    user3 = User(fname='Francis', lname='Kikulwe', email='francis@gmail.com', gender='male',
+        username='franc.kikulwe', phone='54879046', dob='1998-11-9')
+    db.session.add_all([user1, user2, user3])
+    db.session.commit()
+
+    link1 = Link(title='Facebook', url='https://www.facebook.com/johndoe', user=user1)
+    link2 = Link(title='Twitter', url='https://www.twitter.com/johndoe', user=user1)
+    link3 = Link(title='Facebook', url='https://www.facebook.com/janedoe', user=user2)
+    link4 = Link(title='Twitter', url='https://www.twitter.com/janedoe', user=user2)
+    link5 = Link(title='Facebook', url='https://www.facebook.com/francis', user=user3)
+    link6 = Link(title='Twitter', url='https://www.twitter.com/francis', user=user3)
+    link7 = Link(title='LinkedIn', url='https://www.linkedin.com/francis', user=user3)
+    link8 = Link(title='Github', url='https://www.github.com/francis', user=user3)
+    link9 = Link(title='LinkedIn', url='https://www.linkedin.com/johndoe', user=user1)
+    link10 = Link(title='Github', url='https://www.github.com/johndoe', user=user1)
+    link11 = Link(title='LinkedIn', url='https://www.linkedin.com/janedoe', user=user2)
+    link12 = Link(title='Github', url='https://www.github.com/janedoe', user=user2)
+    db.session.add_all([link1, link2, link3, link4, link5, link6, link7, link8, link9, link10, link11, link12])
+    db.session.commit()
+    
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
     fname = db.Column(db.String(120))
     lname = db.Column(db.String(120))
+    username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(350), unique=True)
     gender = db.Column(db.String(10))
     phone = db.Column(db.String(20))
